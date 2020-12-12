e7ff01d817a672af3dbd952b1079c7e1850b871c
Nicholas-Barbosa <nicholas.henrique0604@hotmail.com>
2020-12-12 01:53:23 -0300

    added Owners List and updated controller

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/AbstractMapService.java
@@ -5,11 +5,12 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
-public abstract class AbstractMapService<T, ID>  {
+public abstract class AbstractMapService<T, ID> {
 
 	protected Map<ID, T> map = new HashMap<>();
 
 	Set<T> findAll() {
+
 		return new HashSet<>(map.values());
 	}
 
--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controller/OwnerController.java
@@ -1,14 +1,25 @@
 package guru.springframework.sfgpetclinic.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import guru.springframework.sfgpetclinic.services.OwnerService;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
+	private final OwnerService ownerService;
+
+	public OwnerController(OwnerService ownerService) {
+		super();
+		this.ownerService = ownerService;
+	}
+
 	@RequestMapping({ "", "/index" })
-	public String index() {
+	public String index(Model model) {
+		model.addAttribute("owners",ownerService.findAll());
 		return "owners/index";
 	}
 }
