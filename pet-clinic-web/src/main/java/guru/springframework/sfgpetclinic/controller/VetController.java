6551528f24e7b99fcdda6ef9062e064771d45229
Nicholas-Barbosa <nicholas.henrique0604@hotmail.com>
2020-12-12 02:00:01 -0300

    added Vets List and updated controller

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controller/VetController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controller/VetController.java
@@ -1,13 +1,24 @@
 package guru.springframework.sfgpetclinic.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import guru.springframework.sfgpetclinic.services.VetService;
 
 @Controller
 public class VetController {
 
-	@RequestMapping({ "/vets", "index/vets" })
-	public String listVets() {
+	private final VetService vetService;
+
+	public VetController(VetService vetService) {
+		super();
+		this.vetService = vetService;
+	}
+
+	@RequestMapping({ "/vets", "/index/vets" })
+	public String listVets(Model model) {
+		model.addAttribute("vets", vetService.findAll());
 		return "vets/index";
 	}
 }
