ca5cfb6c424a89a0f714869528902178ba9afd6e
Nicholas-Barbosa <nicholas.henrique0604@hotmail.com>
2020-12-15 15:09:20 -0300

    added properties

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -4,11 +4,37 @@ import java.util.Set;
 
 public class Owner extends Person {
 
-	private Set<Pet>pets;
+	private String address;
+	private String city;
+	private String telephone;
+	private Set<Pet> pets;
+
+	public Owner() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Owner(String address, String city, String telephone, Set<Pet> pets) {
+		super();
+		this.address = address;
+		this.city = city;
+		this.telephone = telephone;
+		this.pets = pets;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public String getTelephone() {
+		return telephone;
+	}
 
 	public Set<Pet> getPets() {
 		return pets;
 	}
-	
-	
+
 }
