3a19ce1a7f33919a04626309d43d0d4a5a075c35
Nicholas-Barbosa <nicholas.henrique0604@hotmail.com>
2020-12-18 18:38:43 -0300

    Updated Base entity

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
@@ -1,7 +1,21 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+/*
+ * Hey JPA, iremos herdas as propriedades dessa classe. Ela nao sera mapeada como uma entidade na database, mas sera de base para as outras mapeadas.
+ */
+@MappedSuperclass
 public class BaseEntity {
 
+	/*
+	 * Sera mapeado a covariantes desta classe.
+	 */
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	public Long getId() {
